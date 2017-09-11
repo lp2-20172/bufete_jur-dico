bece5b2917cea2331b46878ed5db47cf3e09c133
Eversaul1 <ever.saul.2017@gmail.com>
2017-09-11 18:32:00 -0500

    subiendo navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import {Button} from 'react-bootstrap'
+import {Navbar,Nav,NavItem,MenuItem,NavDropdown} from 'react-bootstrap'
 
 export default class Index extends React.Component{
     render(){
         return(<div>
-            <h1> hola mundo </h1>
-            <Button bsStyle="primary">Primary</Button>
+            
+                <Navbar inverse collapseOnSelect>
+                  <Navbar.Header>
+                    <Navbar.Brand>
+                      <a href="#">Sistema de alquiler</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                  </Navbar.Header>
+                  <Navbar.Collapse>
+                    <Nav>
+                      <NavItem eventKey={1} href="#">HOME</NavItem>
+                      <NavItem eventKey={2} href="#">Oficina</NavItem>
+                      <NavDropdown eventKey={3} title="Contactenos" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>Action</MenuItem>
+                        <MenuItem eventKey={3.2}>Another action</MenuItem>
+                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                      </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                      
+                      <NavItem eventKey={2} href="#">Ingresar</NavItem>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Navbar>
+              
+              
+              
 
         </div>);
     }
